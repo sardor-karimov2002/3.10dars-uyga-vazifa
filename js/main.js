41eb518d16898d2a5c2c50b008803d2f1b05d630
sardor-karimov2002 <sardorbekkarimov155@gmail.com>
2022-03-02 00:21:49 +0500

    salom

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var formEl=document.querySelector('.form')
+var inputEl=document.querySelector('.input')
+var the_resultEl=document.querySelector('.the_result')
+
+var couple_sonEL=[]
+var ninetyEl=[]
+
+formEl.addEventListener('submit',evt=>{
+    evt.preventDefault()
+    var inputElValue=inputEl.value ;
+    if(inputElValue%2==0){
+        couple_sonEL.push(inputElValue)
+        console.log(couple_sonEL)
+        the_resultEl.textContent="Bu juft son"
+        return
+    }
+    if(inputElValue%2==1){
+        ninetyEl.push(inputElValue)
+        console.log(ninetyEl)
+        the_resultEl.textContent="Bu toq son"
+        return
+     }
+})
+console.log(couple_sonEL);
+console.log(ninetyEl);
